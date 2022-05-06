5bec2d90cdb67c275a6576b5b284e22f2436c160
Bill Calkins <calkinsc3@gmail.com>
2022-05-06 10:06:11 -0500

    After section 7.7 completed.

--- 07-concurrent-code-with-taskgroup/projects/starter/Sky/ScanModel.swift
+++ 07-concurrent-code-with-taskgroup/projects/starter/Sky/ScanModel.swift
@@ -1,34 +1,5 @@
 /// Copyright (c) 2021 Razeware LLC
-///
-/// Permission is hereby granted, free of charge, to any person obtaining a copy
-/// of this software and associated documentation files (the "Software"), to deal
-/// in the Software without restriction, including without limitation the rights
-/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-/// copies of the Software, and to permit persons to whom the Software is
-/// furnished to do so, subject to the following conditions:
-///
-/// The above copyright notice and this permission notice shall be included in
-/// all copies or substantial portions of the Software.
-///
-/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
-/// distribute, sublicense, create a derivative work, and/or sell copies of the
-/// Software in any work that is designed, intended, or marketed for pedagogical or
-/// instructional purposes related to programming, coding, application development,
-/// or information technology.  Permission for such use, copying, modification,
-/// merger, publication, distribution, sublicensing, creation of derivative works,
-/// or sale is expressly withheld.
-///
-/// This project and source code may use libraries or frameworks that are
-/// released under various Open-Source licenses. Use of those libraries and
-/// frameworks are governed by their own individual licenses.
-///
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-/// THE SOFTWARE.
+
 
 import Foundation
 
@@ -60,6 +31,31 @@ class ScanModel: ObservableObject {
 
   func runAllTasks() async throws {
     started = Date()
+    
+    let scans = await withTaskGroup(of: String.self, body: { [unowned self] group -> [String] in
+      
+      for number in 0..<total {
+        group.addTask {
+          await self.worker(number: number)
+        }
+      }
+      
+      //return strings
+      return await group.reduce(into: [String](), { result, string in
+        result.append(string)
+      })
+    })
+    print(scans)
+  }
+  
+  func worker(number: Int) async -> String {
+    await onScheduled()
+    
+    let task = ScanTask(input: number)
+    let result = await task.run()
+    
+    await onTaskCompleted()
+    return result
   }
 }
 
