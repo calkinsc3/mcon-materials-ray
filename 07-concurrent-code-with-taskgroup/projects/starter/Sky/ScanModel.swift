bf0963e33a3fd76768b139e55340a896f9e951e8
Bill Calkins <calkinsc3@gmail.com>
2022-05-20 09:38:58 -0500

    done with section 7.7

--- 07-concurrent-code-with-taskgroup/projects/starter/Sky/ScanModel.swift
+++ 07-concurrent-code-with-taskgroup/projects/starter/Sky/ScanModel.swift
@@ -2,33 +2,34 @@
 
 
 import Foundation
+import SwiftUI
 
-class ScanModel: ObservableObject {
+final class ScanModel: ObservableObject {
   // MARK: - Private state
   private var counted = 0
   private var started = Date()
-
+  
   // MARK: - Public, bindable state
-
+  
   /// Currently scheduled for execution tasks.
   @MainActor @Published var scheduled = 0
-
+  
   /// Completed scan tasks per second.
   @MainActor @Published var countPerSecond: Double = 0
-
+  
   /// Completed scan tasks.
   @MainActor @Published var completed = 0
-
+  
   @Published var total: Int
-
+  
   @MainActor @Published var isCollaborating = false
-
+  
   // MARK: - Methods
-
+  
   init(total: Int, localName: String) {
     self.total = total
   }
-
+  
   func runAllTasks() async throws {
     started = Date()
     
@@ -40,11 +41,12 @@ class ScanModel: ObservableObject {
         }
       }
       
-      //return strings
       return await group.reduce(into: [String](), { result, string in
         result.append(string)
       })
+      
     })
+    
     print(scans)
   }
   
@@ -66,10 +68,10 @@ extension ScanModel {
     completed += 1
     counted += 1
     scheduled -= 1
-
+    
     countPerSecond = Double(counted) / Date().timeIntervalSince(started)
   }
-
+  
   @MainActor
   private func onScheduled() {
     scheduled += 1
